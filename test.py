7fde69749d772610e4c1859a4f41c9d401502bd8
xiashuo <654017381@qq.com>
2020-06-11 21:59:51 +0800

    扑克牌子顺子

--- test.py
+++ test.py
@@ -31,43 +31,8 @@
 # print(so.lengthOfLongestSubstring("abcabcbb"))
 
 '''
-输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+
 '''
 
 
-class Solution:
-    def GetLeastNumbers_Solution(self, tinput, k):
-        if k > len(tinput):
-            return []
-        tinput.sort()
-        return tinput[:k]
 
-    def GetLeastNumbers_Solution2(self, tinput, k):
-        if k > len(tinput):
-            return []
-        for i in range(k):
-            min_index = i
-            for j in range(i + 1, len(tinput)):
-                if tinput[j] < tinput[min_index]:
-                    min_index = j
-            tinput[i], tinput[min_index] = tinput[min_index], tinput[i]
-        return tinput[:k]
-
-    def GetLeastNumbers_Solution3(self, tinput, k):
-        if k > len(tinput):
-            return []
-        list_k = tinput[:k]
-        list_k.sort()
-        for i in range(k, len(tinput)):
-            j = k - 1
-            while j >= 0 and tinput[i] < list_k[j]:
-                j -= 1
-            list_k.insert(j + 1, tinput[i])
-            list_k.pop()
-
-        return list_k
-
-
-so = Solution()
-res = so.GetLeastNumbers_Solution3([4, 5, 1, 6, 2, 7, 3, 8], 4)
-print(res)
