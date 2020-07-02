f8e1350394f3ad0ba23e684b1dd1b8fa05297d97
xiashuo <654017381@qq.com>
2020-07-02 21:04:01 +0800

    '剑指offer33未完'

--- test.py
+++ test.py
@@ -72,9 +72,16 @@ class Solution:
         return ''.join(str_numbers)
 
     """
-    给定一个二维数组，请按照顺时针
+    把只包含质因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，
+    但14不是，因为它包含质因子7。习惯上我们把1当做是第一个丑数。
+    求按从小到大的顺序的第N个丑数。
     """
 
+    def GetUglyNumber_Solution(self, index):
+        i, j, k = 0, 0, 0
+        ugly_numbers = [1]
+        while len(ugly_numbers) < index:
+            pass
 
 if __name__ == '__main__':
     so = Solution()
